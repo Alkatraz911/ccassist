9764d92d46763baa9597e7f2fb2e74f6312ee09c
unknown <hollysaf@yahoo.com>
2024-02-01 21:55:59 +0300

    added filters ti binanceticketsloader

--- src/binanceSpotActiveTicketsLoader/binanceSpotActiveTicketsLoader.ts
+++ src/binanceSpotActiveTicketsLoader/binanceSpotActiveTicketsLoader.ts
@@ -3,21 +3,23 @@ import { DataSource } from 'typeorm';
 import { SpotTradingTickets } from '../models/SpotTradingTickets.js';
 import { BinanceService } from '../API/BinanceService.js';
 
-
+let filter = ["TOMOUSDT", "TKVUSDT"]
 
 export const binanceSpotActiveTicketsLoader = (dataSource: DataSource) => {
     BinanceService.binanceSpotActiveTicketsLoad()
     .then(data => {
         if(typeof(data) != 'string') {
+            
             data.map(async el => {
-                await dataSource.manager
-                .createQueryBuilder()
-                .insert()
-                .into(SpotTradingTickets)
-                .values({
-                  symbol: el.symbol
-                })
-                .execute();
+                if (!filter.includes(el.symbol))
+                  await dataSource.manager
+                    .createQueryBuilder()
+                    .insert()
+                    .into(SpotTradingTickets)
+                    .values({
+                      symbol: el.symbol,
+                    })
+                    .execute();
             })
         }
 
